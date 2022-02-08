0e2c14022211d70b4d7e20e54c9e4f00958f2753
Jordan Whiteley <thejordanwhiteley@gmail.com>
2022-02-07 21:42:25 -0800

    background and logo

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
