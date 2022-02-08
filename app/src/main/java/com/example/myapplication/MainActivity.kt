0177c969debf5551bbf1f2c7acffd07370f68ffa
Niklaas Cotta <nikcotta@gmail.com>
2022-02-07 22:04:12 -0800

    idk

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.example.myapplication
 
-import android.R
-import android.app.Activity
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
     }
 }
