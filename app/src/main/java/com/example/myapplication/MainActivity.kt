7dc43a022a8e723127a3cfee715aae70aedafe4e
Jordan Whiteley <thejordanwhiteley@gmail.com>
2022-02-15 20:58:30 -0800

    NFC compatibility

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.example.myapplication
 
-import kotlinx.android.synthetic.main.activity_main.*
-import androidx.appcompat.app.AppCompatActivity
+import android.nfc.NfcAdapter
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var nfcAdapter: NfcAdapter? = null
+    private var textViewInfo: TextView? = null
+    private var textViewTagInfo:TextView? = null
+    private var textViewBlock:TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textViewInfo = findViewById(R.id.info)
+        textViewTagInfo = findViewById(R.id.taginfo)
+        textViewBlock = findViewById(R.id.block)
 
-
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (nfcAdapter == null) {
+            Toast.makeText(this,
+                "NFC NOT supported on this devices!",
+                Toast.LENGTH_LONG).show()
+            finish()
+        } else if (!nfcAdapter!!.isEnabled) {
+            Toast.makeText(this,
+                "NFC NOT Enabled!",
+                Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
 }
