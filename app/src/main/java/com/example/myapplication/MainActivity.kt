d846c73afdfc5a7248a8a614ff96e2ba2cfecc0f
Niklaas Cotta <nikcotta@gmail.com>
2022-02-23 18:30:18 -0800

    Added NFC permissions, onNewIntent func

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.media.MediaPlayer
+import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -23,17 +26,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var j = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        /*
+        This function automatically executes on application opening
+         */
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textViewInfo = findViewById(R.id.info)
         textViewTagInfo = findViewById(R.id.taginfo)
-        textViewBlock = findViewById(R.id.block)
+        textViewBlock = findViewById(R.id. block)
 
-        btn = findViewById(R.id.btnRFID)
-        btn!!.setOnClickListener(this)
+        btn = findViewById(R.id.btnRFID)  // Main RFID Scanner Button
+        btn!!.setOnClickListener(this)    // Listen for button to be pressed
 
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-        if (nfcAdapter == null) {
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)  // obtain device's NFCAdapter
+        if (nfcAdapter == null) {   // Device does not have NFCAdapter
             playNFCSound()
             while (i < 4) {
                 Toast.makeText(
@@ -45,7 +51,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
             stopNFCSound()
 //            finish()
-        } else if (!nfcAdapter!!.isEnabled) {
+        } else if (!nfcAdapter!!.isEnabled) {  // Device has NFCAdapter but is turned off
             playNFCSound()
             while (j < 4) {
                 Toast.makeText(
@@ -57,22 +63,45 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
             stopNFCSound()
 //            finish()
-        } else {
+        } else {  // Good to go
             Toast.makeText(this,
                 "NFC IS SUPPORTED, APP IS RUNNING!",
                 Toast.LENGTH_LONG).show()
         }
     }
 
-
-
+    override fun onNewIntent(intent: Intent?) {
+        /*
+        This function will wait for an NFC to be read from the phone. It will then parse the NFC
+        tag info into a message
+         */
+        super.onNewIntent(intent)
+        Log.d("TAG", "New intent received")
+        if (intent != null) {
+            if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
+                intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMessages ->
+                    val messages: List<NdefMessage> = rawMessages.map { it as NdefMessage }
+                    for (message in messages) {
+                        Log.d("TAG", message.toString())
+                    }
+                }
+            }
+        }
+    }
 
     override fun onClick(v: View?) {
+        /*
+        This function will wait for the button to be pressed
+        TODO: Call function to mask RFID from card? Or we set to a switch...
+         */
         btn?.text = "RFID Blocking (ON)"
-        // Call function to mask RFID from card? Or we set to a switch...
     }
 
     private fun playNFCSound() {
+        /*
+        This function will play an error sound.
+        Used when device does not support NFC
+         */
         if (NFCMediaPlayer == null) {
             NFCMediaPlayer = MediaPlayer.create(this, R.raw.error)
             NFCMediaPlayer!!.isLooping = true
@@ -83,6 +112,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun stopNFCSound() {
+        /*
+        This function is used in tandem to playNFCSound()
+         */
         if (NFCMediaPlayer != null) {
             NFCMediaPlayer!!.stop()
             NFCMediaPlayer!!.release()
@@ -91,6 +123,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onStop() {
+        /*
+        TODO: not sure what this does
+         */
         super.onStop()
         if (NFCMediaPlayer != null) {
             NFCMediaPlayer!!.release()
