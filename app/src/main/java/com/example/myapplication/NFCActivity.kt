af3bc4d7b23d6e17aee38bd6990a1bf539576d02
Niklaas Cotta <nikcotta@gmail.com>
2022-03-02 17:34:36 -0800

    added functionality to nfcActivity.kt, removed purple bar

--- app/src/main/java/com/example/myapplication/NFCActivity.kt
+++ app/src/main/java/com/example/myapplication/NFCActivity.kt
@@ -1,19 +1,18 @@
 package com.example.myapplication
 
-import android.app.PendingIntent
-import android.content.Intent
-import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.IsoDep
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.ProgressBar
+import kotlinx.android.synthetic.main.activity_main.*
 
-class NFCActivity : AppCompatActivity() {
-    private lateinit var mNfcAdapter: NfcAdapter
-    private lateinit var mPendingIntent: PendingIntent
+class NFCActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
+    private var nfcAdapter: NfcAdapter? = null
     private lateinit var spinner: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,38 +25,31 @@ class NFCActivity : AppCompatActivity() {
 
         spinner = findViewById(R.id.rfidProgressBar)
 
-        Log.d("TAG", "New Activity started")
-
-        val launchIntent = Intent(this, this.javaClass)
-
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
-        mPendingIntent = PendingIntent.getActivity(this, 0, launchIntent, PendingIntent.FLAG_IMMUTABLE)
-
-        readNfcTag(intent)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
     }
 
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-        spinner.visibility = View.GONE
-        Log.d("TAG", "New intent discovered")
-
-        readNfcTag(intent)
-    }
-
-    private fun readNfcTag(intent: Intent?)
+    override fun onResume()
     {
-        if (intent != null) {
-            if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
-                Log.d("TAG", "New NFC")
-                Log.d("TAG", intent.toString())
-                intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMessages ->
-                    val messages: List<NdefMessage> = rawMessages.map { it as NdefMessage }
-                    for (message in messages) {
-                        Log.d("TAG", message.toString())
-                    }
-                }
-            }
-        }
+        super.onResume()
+        nfcAdapter?.enableReaderMode(this, this, NfcAdapter.FLAG_READER_NFC_A or NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK, null)
+    }
+
+    override fun onPause()
+    {
+        super.onPause()
+        nfcAdapter?.disableReaderMode(this)
+    }
+
+    override fun onTagDiscovered(tag: Tag?) {
+        // spinner.visibility = View.GONE
+        val isoDep = IsoDep.get(tag)
+        isoDep.connect()
+        Log.d("TAG", "onTagDiscovered prompted")
+        val response = isoDep.transceive(Utils.hexStringToByteArray(
+            "A000000003101001"))
+
+        Log.d("TAG", "\nCard Response: " + Utils.toHex(response))
+        isoDep.close()
     }
 }
 
